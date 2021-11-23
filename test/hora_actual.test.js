eee84699efc400b15cf53fdb5291907b72d1d889
devlex <48767282+DevAlexI@users.noreply.github.com>
2021-11-22 19:24:36 -0600

    Commenting failure

--- test/hora_actual.test.js
+++ test/hora_actual.test.js
@@ -4,12 +4,6 @@ const expect = chai.expect;
 const hour = require('../src/hora_actual');
 
 describe('Ejercicio obtener la hora actual', () => {
-    /*describe('La hora actual es correcta ingresando el día actual', () => {
-        it('Tomando en cuenta la hora actual', () => {
-            let today = new Date();
-            expect(hour.getTime('18/11/2021')).to.equal(today.getHours());
-        });
-    });*/
     describe('La hora actual no es correcta', () => {
         it('La hora actual es incorrecta por ingresar mal el día', () => {
             expect(hour.getTime('13/8/2021')).to.equal('fecha mal ingresada');
